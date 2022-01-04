cb3248a9d02c3ebf91a55f155f83041931f1ed04
Fabrizio Guerrieri <fabrguer@gmail.com>
2022-01-04 09:27:12 -0800

    Update to latest version

--- util_functions.py
+++ util_functions.py
@@ -36,91 +36,97 @@ def random_data_avg(distr, pts_nr, avgs_nr):
 # Computes std dev, CI for a distribution. PLots histogram, QQ plot, CI score
 # =============================================================================
 
-def CI_vs_samples(distribution, samples_list, samples_to_plot):
+def CI_vs_samples(distributions, samples_list, samples_to_plot):
 
     avgs_nr = 10000
+    fig, ax = plt.subplots(3,3, figsize=(16,15))
     
-    fig, ax = plt.subplots(1,3, figsize=(16,5))
-    CI_df_cols = ['n','Normal','Exp. Sigma','Sigma','CI_norm','CI_norm_score',
-                  'CI_t','CI_t_score']
-    CI_df = pd.DataFrame(columns=CI_df_cols)
+    results = []
     
-    real_avg = np.mean(random_data_avg(distribution, 1000, avgs_nr))
-
-    # Repeat for each fo the samples number
-    for i, samples in enumerate(samples_list):
-
-        data_avgs = []
-        CI_norm_score = []
-        CI_t_score = []
-
-        # Repeat 10k for each samples number
-        for j in range(avgs_nr):
-            
-            # Generates random data
-            data = random_data(distribution, samples)
-            data_avg = np.mean(data)
-            data_avgs.append(data_avg)
-
-            # Computes the CI assuming normal
-            data_std = np.std(data)
-            CI = 1.96*data_std/np.sqrt(samples)
-            lower = data_avg - CI
-            upper = data_avg + CI
-            CI_norm_score.append(lower <= real_avg <= upper)
-
-            # Computes the CI assuming t-distribution
-            confidence = 0.95
-            std_err = sem(data)
-            h = std_err * t.ppf((1 + confidence) / 2, samples - 1)
-            lower = data_avg - h
-            upper = data_avg + h
-            CI_t_score.append(lower <= real_avg <= upper)
-            
-        # Plots the histogram
-        if samples in samples_to_plot:
-            label = "Samples = {}".format(samples)
-            color = color_lin_gradient(np.array([1,0,0]), np.array([0.2,0,1]), 
-                        len(samples_to_plot))[samples_to_plot.index(samples)]
-            ax[0].hist(data_avgs, bins=50, label=label, color=color);
-            qqplot(np.array(data_avgs), fit=True, line='45', ax=ax[1], 
-                        label=label, color=color);
-
-        # Computes the std deviation
-        is_normal = normaltest(data_avgs)[-1] > 0.05
-        data_std = np.std(random_data(distribution, samples))
-        expected_avgs_std = data_std/np.sqrt(samples)
-        real_avgs_std = np.std(data_avgs)
-            
-        # Update the series and add to the dataframe
-        CI_series = pd.Series(
-            index= CI_df_cols,
-            data = [samples, is_normal, expected_avgs_std, real_avgs_std, 2*CI, 
-                    np.mean(CI_norm_score), 2*h, np.mean(CI_t_score)])
-        CI_df = CI_df.append(CI_series, ignore_index=True)
+    for idx, distribution in enumerate(distributions):
     
-    # Plots the graphs
-    ax[0].set_xlabel("Value")
-    ax[0].set_ylabel("Count")
-    ax[0].set_xlim(0,100);
-    ax[0].legend();
-    ax[1].legend();
-    if distribution == 'exponential':
-        ax[1].set_xlim(-4,4);
-        ax[1].set_ylim(-4,6);
+        CI_df_cols = ['n','Normal','Exp. Sigma','Sigma','CI_norm','CI_norm_score',
+                      'CI_t','CI_t_score']
+        CI_df = pd.DataFrame(columns=CI_df_cols)
 
-    ax[2].set_xlabel("n")
-    ax[2].set_ylabel("CI score")
-    ax[2].plot(samples_list, CI_df['CI_norm_score'].values, 'o', 
-               label="Normal Approx (Eq.2)", color="crimson")
-    ax[2].plot(samples_list, CI_df['CI_t_score'].values, 'o', 
-               label="T-Distr Approx (Eq.3)", color="blue")
-    ax[2].plot(samples_list, len(samples_list)*[0.95], '--', 
-               label='Theoretical CI', color='black')
-    ax[2].set_xscale('log')
-    ax[2].legend();
+        real_avg = np.mean(random_data_avg(distribution, 1000, avgs_nr))
+
+        # Repeat for each fo the samples number
+        for i, samples in enumerate(samples_list):
+
+            data_avgs = []
+            CI_norm_score = []
+            CI_t_score = []
+
+            # Repeat 10k for each samples number
+            for j in range(avgs_nr):
+
+                # Generates random data
+                data = random_data(distribution, samples)
+                data_avg = np.mean(data)
+                data_avgs.append(data_avg)
+
+                # Computes the CI assuming normal
+                data_std = np.std(data)
+                CI = 1.96*data_std/np.sqrt(samples)
+                lower = data_avg - CI
+                upper = data_avg + CI
+                CI_norm_score.append(lower <= real_avg <= upper)
+
+                # Computes the CI assuming t-distribution
+                confidence = 0.95
+                std_err = sem(data)
+                h = std_err * t.ppf((1 + confidence) / 2, samples - 1)
+                lower = data_avg - h
+                upper = data_avg + h
+                CI_t_score.append(lower <= real_avg <= upper)
+
+            # Plots the histogram
+            if samples in samples_to_plot:
+                label = "n = {}".format(samples)
+                color = color_lin_gradient(np.array([1,0,0]), np.array([0.2,0,1]), 
+                            len(samples_to_plot))[samples_to_plot.index(samples)]
+                ax[idx,0].hist(data_avgs, bins=50, label=label, color=color);
+                qqplot(np.array(data_avgs), fit=True, line='45', ax=ax[idx,1], 
+                            label=label, color=color);
+
+            # Computes the std deviation
+            is_normal = normaltest(data_avgs)[-1] > 0.05
+            data_std = np.std(random_data(distribution, samples))
+            expected_avgs_std = data_std/np.sqrt(samples)
+            real_avgs_std = np.std(data_avgs)
+
+            # Update the series and add to the dataframe
+            CI_series = pd.Series(
+                index= CI_df_cols,
+                data = [samples, is_normal, expected_avgs_std, real_avgs_std, 2*CI, 
+                        np.mean(CI_norm_score), 2*h, np.mean(CI_t_score)])
+            CI_df = CI_df.append(CI_series, ignore_index=True)
+
+        # Plots the graphs
+        ax[idx,0].set_xlabel("Value")
+        ax[idx,0].set_ylabel("Count")
+        ax[idx,0].set_xlim(0,100);
+        ax[idx,0].legend();
+        ax[idx,1].legend();
+        if distribution == 'exponential':
+            ax[idx,1].set_xlim(-4,4);
+            ax[idx,1].set_ylim(-4,6);
+
+        ax[idx,2].set_xlabel("n")
+        ax[idx,2].set_ylabel("CI score")
+        ax[idx,2].plot(samples_list, CI_df['CI_norm_score'].values, 'o', 
+                   label="Normal Approx (Eq.2)", color="crimson")
+        ax[idx,2].plot(samples_list, CI_df['CI_t_score'].values, 'o', 
+                   label="T-Distr Approx (Eq.3)", color="blue")
+        ax[idx,2].plot(samples_list, len(samples_list)*[0.95], '--', 
+                   label='Theoretical CI', color='black')
+        ax[idx,2].set_xscale('log')
+        ax[idx,2].legend();
     
-    return CI_df
+        results.append(CI_df)
+    
+    return results
 
 
 # Return a list of n colors from start_color to finish_color
